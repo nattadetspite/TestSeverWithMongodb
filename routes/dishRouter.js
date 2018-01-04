534a0d4352265b6fd0e40b83036805ffeeaf385b
Nattadet Chinthanathatset <ntd.ctnts@gmail.com>
2018-01-04 17:37:30 +0700

    population module

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
 .get((req,res,next) => {
   Dishes.find({})
+  .populate('comments.author')
   .then((dishes) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -44,8 +45,9 @@ dishRouter.route('/')
 });
 
 dishRouter.route('/:dishId')
-.get(authenticate.verifyUser, (req,res,next) => {
+.get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comments.author')
   .then((dish) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -82,6 +84,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comment.author')
   .then((dish) => {
     if (dish != null) {
       res.statusCode = 200;
@@ -100,6 +103,7 @@ dishRouter.route('/:dishId/comments')
   Dishes.findById(req.params.dishId)
   .then((dish) => {
     if (dish != null) {
+      req.body.author = req.user._id;
       dish.comments.push(req.body);
       dish.save()
       .then((dish) => {
@@ -146,6 +150,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comments.author')
   .then((dish) => {
     if (dish != null && dish.comments.id(req.params.commentId) != null) {
       res.statusCode = 200;
